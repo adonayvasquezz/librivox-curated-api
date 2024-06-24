7ab369c61f6d291cb7678897b0c5f32ad5e79259
Adonay Vasquez <adonay16.vz@gmail.com>
2024-06-23 22:28:02 -0600

    project init

---
+++ src/presentation/server.ts
@@ -0,0 +1,35 @@
+import express, { Router } from "express";
+import compression from "compression";
+import path from "path";
+
+interface Options {
+  port: number;
+  routes: Router;
+  public_path?: string;
+}
+
+export class Server {
+  private app = express();
+  private readonly port: number;
+  private readonly routes: Router;
+
+  constructor(options: Options) {
+    const { port, routes } = options;
+    this.port = port;
+    this.routes = routes;
+  }
+
+  async start() {
+    //* Middlewares
+    this.app.use(express.json()); // raw
+    this.app.use(express.urlencoded({ extended: true })); // x-www-form-urlencoded
+    this.app.use(compression());
+
+    //* Routes
+    this.app.use(this.routes);
+
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
